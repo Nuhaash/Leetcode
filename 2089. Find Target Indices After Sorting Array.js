9b7ddda55079402439968346f36928bf1de0dc5e
Nuhaash <nuhash.cf.bd@gmail.com>
2023-03-12 16:11:48 -0700

    first commit

---
+++ 2089. Find Target Indices After Sorting Array.js
@@ -0,0 +1,18 @@
+var targetIndices = function(nums, target) {
+    
+    nums = nums.sort((a,b) =>{
+        return a - b
+    })
+
+    let result = []
+    for (let i = 0; i < nums.length; i++){
+        if(nums[i] == target){
+            result.push(i);
+        }
+        }
+        return result;
+    };
+
+
+
+console.log(targetIndices[1,2,5,2,3]);
